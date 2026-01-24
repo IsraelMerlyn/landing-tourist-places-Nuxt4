a203b916dfe7e7ba9096310c263198739378f3b0
Israel Merlyn <merlyn142a@gmail.com>
2026-01-24 15:59:27 -0600

    tasks Imp,ementando refactorizacion de Datos

---
+++ app/type/index.ts
@@ -0,0 +1,13 @@
+
+export interface Destino {
+    id: number;
+    titulo: string;
+    categoria: string;
+    descripcion_corto: string;
+    descripcion_completa: string;
+    imagen: string;
+    ubicacion: string;
+    precio: string;
+    tips: string[];
+    galeria: string[];
+}
