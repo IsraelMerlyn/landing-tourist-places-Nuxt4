98e4825ab3870c88eb241790c8f6141e63d4d0b2
Israel Merlyn <merlyn142a@gmail.com>
2026-01-24 23:55:20 -0600

    feat(auth): implementacion completa de login, middleware de seguridad y dashboard base

--- app/pages/admin/dashboard.vue
+++ app/pages/admin/dashboard.vue
@@ -3,9 +3,44 @@ const client = useSupabaseClient()
 const user = useSupabaseUser()
 const router = useRouter()
 
+const email = ref('')
+const password = ref('')
+const loading = ref(false)
+const errorMsg = ref('')
+const showPassword = ref(false) // Estado para el ojito
+definePageMeta({
+  middleware: 'auth' // Nombre del archivo que creamos en el paso 1
+})
+// Redirigir si ya hay sesión
+watchEffect(() => {
+  if (user.value) {
+    router.push('/admin/dashboard')
+  }
+})
 const logout = async () => {
   await client.auth.signOut()
   router.push('/login')
+}
+const handleLogin = async () => {
+  loading.value = true
+  errorMsg.value = ''
+  
+  const { error } = await client.auth.signInWithPassword({
+    email: email.value,
+    password: password.value
+  })
+
+  if (error) {
+    loading.value = false
+    // Mensaje amigable dependiendo del error
+    if (error.message.includes('Invalid login credentials')) {
+      errorMsg.value = 'Correo o contraseña incorrectos.'
+    } else if (error.message.includes('Email not confirmed')) {
+      errorMsg.value = 'Debes confirmar tu correo o desactivar la confirmación en Supabase.'
+    } else {
+      errorMsg.value = error.message
+    }
+  }
 }
 </script>
 
